b29f1dd7b2cac66c6f4c2b22163eb68ff47186d1
lindsey raffa and Mao Thygesen <maothygesen@gmail.com>
2016-02-18 11:54:47 -0800

    create template files and add outline

---
+++ Template Foundation/js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+      $("#owl").owlCarousel({
+          navigation : true,
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          singleItem:true
+      });
+    });
